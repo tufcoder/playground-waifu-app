38934c44a483276e4d4ca73f35ba9357c872e5e9
Oswaldo Castro <oswaldogpc@gmail.com>
2025-02-12 12:12:21 -0300

    generate waifus in a grid

--- nodejs-backend/server.js
+++ nodejs-backend/server.js
@@ -4,22 +4,42 @@ const cors = require('cors')
 
 const app = express()
 const PORT = 1337
+
 const URL_API = 'https://api.waifu.pics'
+const MANY = 'many'
+const TYPE = 'sfw'
+const CATEGORY = 'waifu'
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/waifu', async (req, res) => {
-    try {
-        const response = await axios.get(`${URL_API}/sfw/waifu`)
-        res.send(response.data)
-    } catch (error) {
-        res.status(500).send({
-            error: 'Server error: fetching waifu pics api'
-        })
-    }
+  try {
+    const response = await axios.get(
+      `${URL_API}/${TYPE}/${CATEGORY}`
+    )
+    res.send(response.data)
+  } catch (error) {
+    res.status(500).send({
+      error: 'Server error: fetching waifu pics api'
+    })
+  }
+})
+
+app.post('/api/many/waifu', async (req, res) => {
+  try {
+    const response = await axios.post(
+      `${URL_API}/${MANY}/${TYPE}/${CATEGORY}`,
+      {}
+    )
+    res.send(response.data)
+  } catch (error) {
+    res.status(500).send({
+      error: 'Server error: fetching waifu pics api'
+    })
+  }
 })
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`)
 })
--- react-frontend-js/src/App.js
+++ react-frontend-js/src/App.js
@@ -1,44 +1,60 @@
 import { useState } from 'react'
+import axios from 'axios'
 import Card from './components/Card';
 import './App.css'
 
 function App() {
-  const [waifu, setWaifu] = useState(null)
+  // const [waifu, setWaifu] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [waifus, setWaifus] = useState([])
   // const DOTNET_BACKEND = 'http://localhost:5280/api/waifu'
   // const MINIMAL_DOTNET_BACKEND = 'http://localhost:5100/api/waifu'
-  const NODEJS_BACKEND = 'http://localhost:1337/api/waifu'
+  // const NODEJS_BACKEND = 'http://localhost:1337/api/waifu'
+  const NODEJS_BACKEND_MANY = 'http://localhost:1337/api/many/waifu'
 
-  const fetchWaifu = async () => {
+  // const fetchWaifu = async () => {
+  //   setLoading(true)
+  //   try {
+  //     const response = await fetch(NODEJS_BACKEND)
+  //     if (!response.ok) {
+  //       console.error(`HTTP error! status: ${response.status}`)
+  //     }
+  //     const data = await response.json()
+  //     setWaifu(data.url)
+  //   } catch (error) {
+  //     console.error(`Error fetching waifu: ${NODEJS_BACKEND}`, error)
+  //     setWaifu(null)
+  //   } finally {
+  //     setLoading(false)
+  //   }
+  // }
+
+  const fetchManyWaifus = async () => {
     setLoading(true)
     try {
-      const response = await fetch(NODEJS_BACKEND)
-      if (!response.ok) {
-        console.error(`HTTP error! status: ${response.status}`)
-      }
-      const data = await response.json()
-      setWaifu(data.url)
+      const response = await axios.post(NODEJS_BACKEND_MANY, {})
+      setWaifus(response.data.files)
     } catch (error) {
-      console.error(`Error fetching waifu: ${NODEJS_BACKEND}`, error)
-      setWaifu(null)
+      console.error(`Error fetching waifu: ${NODEJS_BACKEND_MANY}`, error)
+      setWaifus(null)
     } finally {
       setLoading(false)
     }
   }
 
-  // const fetchWaifu = async () => {
-  //   setLoading(true)
-  //   await new Promise(resolve => setTimeout(resolve, 1000))
-  //   setWaifu('https://i.waifu.pics/BwHGNNK.png')
-  //   setLoading(false)
-  // }
-
   return (
     <div className="App">
       <h1>Waifus Generator</h1>
-      <Card waifu={waifu} loading={loading} />
-      <button className='button' onClick={fetchWaifu} disabled={loading}>
-        {loading ? 'Loading...' : 'Generate New Waifu'}
+      <div className='waifus-container'>
+        {
+          waifus &&
+          waifus.map(waifu => (
+            <Card key={waifu} waifu={waifu} loading={loading} />
+          ))
+        }
+      </div>
+      <button className='button' onClick={fetchManyWaifus} disabled={loading}>
+        {loading ? 'Loading...' : 'Generate'}
       </button>
     </div>
   )
